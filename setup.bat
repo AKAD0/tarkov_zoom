e3fd49abf219542fd0a01420a3a97f78c34f135a
AKAD0 <onixon2010@gmail.com>
2025-05-10 04:10:42 +0300

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.
      On branch master
    
      Initial commit
    
      Changes to be committed:
     	new file:   .gitignore
     	new file:   1.py
     	new file:   run.bat
     	new file:   setup.bat

---
+++ setup.bat
@@ -0,0 +1,16 @@
+@echo off
+REM set current directory
+set DIR=%~dp0
+cd /d %DIR%
+
+REM install libs
+"python.exe" -m venv venv
+call .\venv\Scripts\activate.bat
+pip install pynput pyautogui
+
+REM add .gitignore file
+cd /d %DIR%
+echo. > .gitignore
+echo venv/ >> .gitignore
+
+pause
