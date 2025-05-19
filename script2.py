6adfe68b4f6ea5482b1f12b3111eb794a42ba2e9
AKAD0 <onixon2010@gmail.com>
2025-05-19 06:05:33 +0300

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.
      On branch master
      Your branch is ahead of 'origin/master' by 8 commits.
        (use "git push" to publish your local commits)
    
      Changes to be committed:
     	modified:   script2.py

--- script2.py
+++ script2.py
@@ -2,6 +2,7 @@ import ctypes
 from ctypes import wintypes
 import keyboard
 import time
+import asyncio
 
 
 def test_gamma_contrast_set(gamma_value, contrast_value):
@@ -57,29 +58,58 @@ def zoom_init(zoom_level):
 
 
 
-zoom_level = 1.0
-gamma = 4           # 1.0 = no change
-contrast = 1.05     # 1.0 = no change
-press_start_time, pause, magnification = zoom_init(zoom_level)
-test_gamma_contrast_set(gamma, contrast)
+async def handle_zoom(zoom_level, magnification, pause, press_start_time):
+    while True:
+        if keyboard.is_pressed('shift'): 
+            if press_start_time is None:
+                press_start_time = time.time()
+
+            if time.time() - press_start_time >= pause:
+                zoom_level = 4
+                x = int(960*((16-(16/zoom_level))/16))
+                y = int((x/16)*9)
+                set_zoom_level(zoom_level, x, y, magnification)
+        else:
+            press_start_time = None
+            if zoom_level != 1.0:  
+                zoom_level = 1.0  
+                set_zoom_level(zoom_level, 0, 0, magnification)
+
+        await asyncio.sleep(0.01)  # Prevent high CPU usage
+
+
+async def handle_flash(gamma, contrast, flash_gamma, flash_contrast):
+    flash=False
+    while True:
+        if keyboard.is_pressed('v') and flash==False:
+            test_gamma_contrast_set(flash_gamma, flash_contrast)
+            flash=True
+            time.sleep(0.25)
+        elif keyboard.is_pressed('v') and flash==True:
+            test_gamma_contrast_set(gamma, contrast)
+            flash=False
+            time.sleep(0.25)
+
+        await asyncio.sleep(0.01)  # Prevent high CPU usage
+
+
+
+async def main():
+    zoom_level = 1.0
+    gamma = 4           # 1.0 = no change
+    contrast = 1.05     # 1.0 = no change
+    flash_gamma = 2     # 1.0 = no change
+    flash_contrast = 1.00  # 1.0 = no change
+
+    press_start_time, pause, magnification = zoom_init(zoom_level)
+    test_gamma_contrast_set(gamma, contrast)
+
+    await asyncio.gather(
+        handle_zoom(zoom_level, magnification, pause, press_start_time),
+        handle_flash(gamma, contrast, flash_gamma, flash_contrast)
+    )
 
 
 
 
-
-while True:
-    if keyboard.is_pressed('shift'): 
-        if press_start_time is None:
-            press_start_time = time.time()
-
-        if time.time() - press_start_time >= pause:
-            zoom_level = 4
-            x = int(960*((16-(16/zoom_level))/16))
-            y = int((x/16)*9)
-            set_zoom_level(zoom_level, x, y, magnification)
-
-    else:
-        press_start_time = None
-        if zoom_level != 1.0:  
-            zoom_level = 1.0  
-            set_zoom_level(zoom_level, 0, 0, magnification)
+asyncio.run(main())
